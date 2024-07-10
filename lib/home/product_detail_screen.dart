a10f0762849da3a8084106b5f83c33978f6b195c
seongukHEO <heoseonguk3@gmail.com>
2024-07-11 06:39:26 +0900

    feat : 제품 상세 화면 구현

--- lib/home/product_detail_screen.dart
+++ lib/home/product_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({super.key});
@@ -10,6 +11,97 @@ class ProductDetailScreen extends StatefulWidget {
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("패캠 제품 상세"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 320,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                    ),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                            child: Text("할인중", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("패캠 플러터", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+                            PopupMenuButton(itemBuilder: (context){
+                              return [
+                                PopupMenuItem(child: Text("리뷰등록"))
+                              ];
+                            })
+                          ],
+                        ),
+                        Text("제품 상세 정보"),
+                        Text("상세 상세"),
+                        SizedBox(height: 18,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("1000000원"),
+                            Spacer(),
+                            Icon(Icons.star, color: Colors.orange,),
+                            Text("4.5")
+                          ],
+                        )
+
+                      ],
+                    ),
+                  ),
+                  DefaultTabController(length: 2, child: Column(
+                    children: [
+                      TabBar(tabs: [
+                        Tab(text: "제품 상세",),
+                        Tab(text: "리뷰",),
+                      ]),
+                      Container(
+                        height: 500,
+                        child: TabBarView(
+                          children: [
+                            Container(
+                              child: Text("제품 상세"),
+                            ),
+                            Container(
+                              child: Text("리뷰"),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
